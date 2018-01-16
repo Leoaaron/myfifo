1c8d2c66a3e42b0be54bd7c5d52d9fda3ce99722
Leonix <denglun2013@163.com>
2018-01-16 19:38:13 +0800

    add one test case for push data out of fifo size

--- test.c
+++ test.c
@@ -18,5 +18,14 @@ int main(int argc, char *argv[])
     myfifo_out(&fifo, buf, sizeof(buf));
     printf("read from myfifo :%s\n", buf);
 
+    for(int i=0;i<12;i++)
+        myfifo_in(&fifo, str, strlen(str));
+    
+    unsigned int l = 0;
+    char bbuf[256] = "";
+    l = myfifo_out(&fifo, bbuf, sizeof(bbuf));
+    printf("l=%d, data:%s\n", l, bbuf);
+   
+    myfifo_free(&fifo);
     return 0;
 }
